8481222fd0e4c66565f2af9cc1fdf831997c06b6
ParisNeo <aloui.seifeddine@gmail.com>
2023-04-04 00:07:01 +0200

    Added cool installer/uninstaller for windows/ linux and macos

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-@echo off
+#!/usr/bin/bash
 
 echo                            G#........................G                          
 echo                         ............*....................                       
@@ -34,99 +34,68 @@ echo                           G:.*.                   ...:G
 echo echo PYLLAMACPP Install tool
 echo This will install the python bindings for llamacpp.
 
-pause
-REM Check if Python is installed
-set /p="Checking for python..." <nul
-where python >nul 2>&1 
-if %ERRORLEVEL% neq 0 (
-    echo Python is not installed. Would you like to install Python? [Y/N]
-    set /p choice=
-    if /i "%choice%" equ "Y" (
-        REM Download Python installer
-        echo Downloading Python installer...
-        powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe' -OutFile 'python.exe'"
-        REM Install Python
-        echo Installing Python...
-        python.exe /quiet /norestart
-    ) else (
-        echo Please install Python and try again.
-        pause
-        exit /b 1
-    )
-) else (
-    echo OK
-)
+# Install Python 3.10 and pip
+echo -n "Checking for python3.10..."
+if command -v python3.10 > /dev/null 2>&1; then
+  echo "OK"
+else
+  read -p "Python3.10 is not installed. Would you like to install Python3.10? [Y/N] " choice
+  if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
+    echo "Installing Python3.10..."
+    sudo apt update
+    sudo apt install -y python3.10 python3.10-venv
+  else
+    echo "Please install Python3.10 and try again."
+    exit 1
+  fi
+fi
 
-REM Check if pip is installed
-set /p="Checking for pip..." <nul
-python -m pip >nul 2>&1
-if %ERRORLEVEL% neq 0 (
-    echo Pip is not installed. Would you like to install pip? [Y/N]
-    set /p choice=
-    if /i "%choice%" equ "Y" (
-        REM Download get-pip.py
-        echo Downloading get-pip.py...
-        powershell -Command "Invoke-WebRequest -Uri 'https://bootstrap.pypa.io/get-pip.py' -OutFile 'get-pip.py'"
-        REM Install pip
-        echo Installing pip...
-        python get-pip.py
-    ) else (
-        echo Please install pip and try again.
-        pause
-        exit /b 1
-    )
-) else (
-    echo OK
-)
+# Install venv module
+echo -n "Checking for venv module..."
+if python3.10 -m venv env > /dev/null 2>&1; then
+  echo "OK"
+else
+  read -p "venv module is not available. Would you like to install it? [Y/N] " choice
+  if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
+    echo "Installing venv module..."
+    sudo apt update
+    sudo apt install -y python3.10-venv
+  else
+    echo "Please install venv module and try again."
+    exit 1
+  fi
+fi
 
-REM Check if venv module is available
-set /p="Checking for venv..." <nul
-python -c "import venv" >nul 2>&1
-if %ERRORLEVEL% neq 0 (
-    echo venv module is not available. Would you like to upgrade Python to the latest version? [Y/N]
-    set /p choice=
-    if /i "%choice%" equ "Y" (
-        REM Upgrade Python
-        echo Upgrading Python...
-        python -m pip install --upgrade pip setuptools wheel
-        python -m pip install --upgrade --user python
-    ) else (
-        echo Please upgrade your Python installation and try again.
-        pause
-        exit /b 1
-    )
-) else (
-    echo OK
-)
+# Create a new virtual environment
+echo -n "Creating virtual environment..."
+python3.10 -m venv env
+if [ $? -ne 0 ]; then
+  echo "Failed to create virtual environment. Please check your Python installation and try again."
+  exit 1
+else
+  echo "OK"
+fi
 
-REM Create a new virtual environment
-set /p="Creating virtual environment ..." <nul
-python -m venv env
-if %ERRORLEVEL% neq 0 (
-    echo Failed to create virtual environment. Please check your Python installation and try again.
-    pause
-    exit /b 1
-) else (
-    echo OK
-)
+# Activate the virtual environment
+echo -n "Activating virtual environment..."
+source env/bin/activate
+echo "OK"
 
-REM Activate the virtual environment
-set /p="Activating virtual environment ..." <nul
-call env\Scripts\activate.bat
-echo OK
-REM Install the required packages
-echo Installing requirements ...
-python -m pip install pip --upgrade
-python -m pip install -r requirements.txt
-if %ERRORLEVEL% neq 0 (
-    echo Failed to install required packages. Please check your internet connection and try again.
-    pause
-    exit /b 1
-)
+# Install the required packages
+echo "Installing requirements..."
+export DS_BUILD_OPS=0
+export DS_BUILD_AIO=0
+python3.10 -m pip install pip --upgrade
+python3.10 -m pip install -r requirements.txt
 
-echo Virtual environment created and packages installed successfully.
+if [ $? -ne 0 ]; then
+  echo "Failed to install required packages. Please check your internet connection and try again."
+  exit 1
+fi
+
+echo "Virtual environment created and packages installed successfully."
+
 
 echo Installing pyLLamacpp
 pip install -e .
-pause
-exit /b 0
+exit 0
