c7e1d66a7e8c3ab1898f501145618d5720151274
ParisNeo <aloui.seifeddine@gmail.com>
2023-04-04 00:09:51 +0200

    fixed uninstall

--- uninstall.bat
+++ uninstall.bat
@@ -37,7 +37,7 @@ if /i "%choice%" equ "Y" (
     REM Download Python installer
     echo -n 
     set /p="Removing virtual environment..." <nul
-    powershell -Command "rm env -y"
+    powershell -Command "rd /s /q"
     echo OK
     pause
 ) else (
